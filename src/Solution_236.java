a9c2aadb33b0d087e138a1b54c133a3b8cbd0ee2
songguanzhou <song.guan@husky.neu.edu>
2018-12-04 15:36:43 -0500

    n

--- src/Solution_236.java
+++ src/Solution_236.java
@@ -6,11 +6,13 @@ public class Solution_236 {
     }
     TreeNode left = lowestCommonAncestor(root.left, p, q);
     TreeNode right = lowestCommonAncestor(root.right, p, q);
+    //if both side is null, then p, q is not exist in the tree
     if (left == null && right == null) {
       return null;
     } else if (left != null && right != null) {
+      //if both is not null, then p,q exist in each side, and root is the common ancestor.
       return root;
-    } else {
+    } else {//if only one is not null, then must be it.
       return left == null ? right : left;
     }
   }
