09459a19492490df8e01be5ad054e4370c278d5a
Guanzhou Song <guanzhou.song@gmail.com>
2018-12-04 09:04:07 -0500

    n

---
+++ src/Solution_235.java
@@ -0,0 +1,12 @@
+public class Solution_235 {
+
+  public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    if (root.val > p.val && root.val > q.val) {
+      return lowestCommonAncestor(root.left, p, q);
+    }
+    if (root.val < p.val && root.val < q.val) {
+      return lowestCommonAncestor(root.right, p, q);
+    }
+    return root;
+  }
+}
