70dd691bbc1d80c6a93e9888956fb35bc55faae0
songguanzhou <song.guan@husky.neu.edu>
2018-12-03 21:53:37 -0500

    n

---
+++ test/Solution_215Test.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class Solution_215Test {
+
+  @Test
+  public void test() {
+    Solution_215 s = new Solution_215();
+    int[] input = {7, 1, 5, 3, 6, 4};
+    System.out.println(s.findKthLargest(input, 2));
+  }
+
+}
