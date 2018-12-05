2645045e45cdedadde1a6c9e2a655cc902fb8791
songguanzhou <song.guan@husky.neu.edu>
2018-12-05 15:26:28 -0500

    n

--- src/Solution_240.java
+++ src/Solution_240.java
@@ -7,8 +7,14 @@ public class Solution_240 {
     int row = 0;
     int col = matrix[0].length - 1;
     while (col >= 0 && row <= matrix.length - 1) {
-
+      if (matrix[row][col] == target) {
+        return true;
+      } else if (matrix[row][col] < target) {
+        row++;
+      } else {
+        col--;
+      }
     }
-
+    return false;
   }
 }
